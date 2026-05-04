1d231e9547979e63c2fe3180658d06664224406e
OlesyaAsanova88 <asa88.88@bk.ru>
2026-05-04 11:27:08 +0300

    dist faile

--- dist/bundle.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";new Swiper(".mySwiper",{effect:"coverflow",grabCursor:!0,centeredSlides:!0,slidesPerView:"auto",coverflowEffect:{rotate:50,stretch:0,depth:100,modifier:1,slideShadows:!0},pagination:{el:".swiper-pagination"}})})();
